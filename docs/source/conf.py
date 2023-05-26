088ae4340bb1f2f0de7d54e2a57245104fa1a2cd
HollandCasino <120786034+HollandCasino@users.noreply.github.com>
2023-05-26 14:46:34 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Hackable ESP Device'
+copyright = '2023, pieterjm'
+author = 'Cas Roozendaal'
 
 release = '0.1'
 version = '0.1.0'
